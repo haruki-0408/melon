a0755b2f0e7065f3b4fc5d5fc02c3031bc177c1b
a3a <94882349+haruki-0408@users.noreply.github.com>
2024-12-28 01:28:31 +0900

    [WIP]Apigateway+ EventBridge を用いて進捗通知と実行の両方を兼ねるように調整中
    - WorkflowProgressRecorder 役割と処理内容変更
    - WorkflowProgressNotifier 進捗通知の関数として定義

---
+++ melon/functions/stepfunctions/workflow_progress_notifier/app.py
@@ -0,0 +1,66 @@
+from aws_lambda_powertools import Logger, Tracer
+import os
+import json
+import requests
+
+# envパラメータ
+NEXTJS_API_ENDPOINT = os.environ["NEXTJS_API_ENDPOINT"]  # Next.jsのAPIエンドポイントのベースURL
+
+logger = Logger()
+tracer = Tracer()
+
+@logger.inject_lambda_context(log_event=True)
+@tracer.capture_lambda_handler
+def lambda_handler(event, context):
+    """
+    DynamoDB Streamからのイベントを受け取り、
+    Next.jsのSSEエンドポイントに進捗情報を送信するLambda関数
+    """
+    try:
+        # DynamoDB Streamからのレコードを処理
+        for record in event['Records']:
+            # INSERT または MODIFY イベントのみ処理
+            if record['eventName'] in ['INSERT', 'MODIFY']:
+                # DynamoDBのNewImageから項目を取得
+                new_image = record['dynamodb']['NewImage']
+                
+                # DynamoDBのデータ形式を通常の辞書形式に変換
+                item = {
+                    'workflow_id': new_image['workflow_id']['S'],
+                    'timestamp#order': new_image['timestamp#order']['S'],
+                    'state_name': new_image['state_name']['S'],
+                    'execution_id': new_image['execution_id']['S'],
+                    'status': new_image['status']['S']
+                }
+                
+                # error_detailsが存在する場合のみ追加
+                # if 'error_details' in new_image:
+                #     item['error_details'] = json.loads(new_image['error_details']['S'])
+
+                # workflow_id
+                workflow_id = item['workflow_id']
+
+                # Next.jsのSSEエンドポイントにPOSTリクエストを送信
+                sse_endpoint = f"{NEXTJS_API_ENDPOINT}/api/notify"
+                response = requests.post(
+                    sse_endpoint,
+                    json=item,
+                    headers={'Content-Type': 'application/json'}
+                )
+                
+                if response.status_code != 200:
+                    logger.error(f"Failed to send data to SSE endpoint: {response.text}")
+                    raise Exception("Failed to send data to SSE endpoint")
+
+        return {
+            'status_code': 200,
+            'body': json.dumps({'message': 'Progress notifications sent successfully'})
+        }
+    except Exception as e:
+        error = {
+            "error_type": type(e).__name__,
+            "error_message": str(e),
+            "payload": event
+        }
+        logger.exception(error)
+        raise e
--- melon/functions/stepfunctions/workflow_progress_recorder/app.py
+++ melon/functions/stepfunctions/workflow_progress_recorder/app.py
@@ -1,61 +1,48 @@
 from aws_lambda_powertools import Logger, Tracer
 import os
 import json
-import requests
-
+from utilities import put_item_to_dynamodb
+from datetime import datetime, timezone, timedelta
 # envパラメータ
-NEXTJS_API_ENDPOINT = os.environ["NEXTJS_API_ENDPOINT"]  # Next.jsのAPIエンドポイントのベースURL
+DYNAMO_DB_WORKFLOW_PROGRESS_TABLE = os.environ["DYNAMO_DB_WORKFLOW_PROGRESS_TABLE"]  # DynamoDBのテーブル名
 
 logger = Logger()
 tracer = Tracer()
+JST = timezone(timedelta(hours=+9), 'JST')
 
 @logger.inject_lambda_context(log_event=True)
 @tracer.capture_lambda_handler
 def lambda_handler(event, context):
     """
-    DynamoDB Streamからのイベントを受け取り、
-    Next.jsのSSEエンドポイントに進捗情報を送信するLambda関数
+    進捗をDynamoDBに保存し更新する関数
     """
-    logger.info(event)
     try:
-        # DynamoDB Streamからのレコードを処理
-        for record in event['Records']:
-            # INSERT または MODIFY イベントのみ処理
-            if record['eventName'] in ['INSERT', 'MODIFY']:
-                # DynamoDBのNewImageから項目を取得
-                new_image = record['dynamodb']['NewImage']
-                
-                # DynamoDBのデータ形式を通常の辞書形式に変換
-                item = {
-                    'workflow_id': new_image['workflow_id']['S'],
-                    'timestamp#order': new_image['timestamp#order']['S'],
-                    'state_name': new_image['state_name']['S'],
-                    'execution_id': new_image['execution_id']['S'],
-                    'status': new_image['status']['S']
-                }
-                
-                # error_detailsが存在する場合のみ追加
-                # if 'error_details' in new_image:
-                #     item['error_details'] = json.loads(new_image['error_details']['S'])
+        workflow_id = event['workflow_id']
+        request_id = event['request_id']
+        order = event['order']
+        status = event['status']
+        state_name = event['state_name']
 
-                # workflow_id
-                workflow_id = item['workflow_id']
+        # JSTのタイムスタンプを取得
+        timestamp = datetime.now(JST).isoformat()
 
-                # Next.jsのSSEエンドポイントにPOSTリクエストを送信
-                sse_endpoint = f"{NEXTJS_API_ENDPOINT}/api/notify"
-                response = requests.post(
-                    sse_endpoint,
-                    json=item,
-                    headers={'Content-Type': 'application/json'}
-                )
-                
-                if response.status_code != 200:
-                    logger.error(f"Failed to send data to SSE endpoint: {response.text}")
-                    raise Exception("Failed to send data to SSE endpoint")
+        item = {
+            'workflow_id': workflow_id,
+            'timestamp#order': timestamp + '#' + str(order),
+            'state_name': state_name,
+            'status': status,
+            'request_id': request_id
+        }
+
+        # DynamoDBに保存
+        put_item_to_dynamodb(
+            TableName=DYNAMO_DB_WORKFLOW_PROGRESS_TABLE,
+            Item=item
+        )
 
         return {
             'status_code': 200,
-            'body': json.dumps({'message': 'Progress notifications sent successfully'})
+            'body': json.dumps({'message': 'Progress recorded successfully'})
         }
     except Exception as e:
         error = {
